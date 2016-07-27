730a51652f757e3700dc5cd9bfe21401492adfc7
jakeMalwitz <jakeMalwitz@Jakes-MacBook-Pro.local>
2016-07-27 17:19:54 -0500

    please

--- peer_JS2_01.js
+++ peer_JS2_01.js
@@ -1,35 +1,105 @@
-/*
-1. Start with the number 42 and set that equal to 'value'
 
-2. Create condition logic to check if the value is great or equal to 53
-2-1. If true, add 42 to 'value'
-2-2. If false, subtract 13 from 'value'
+//1. Start with the number 42 and set that equal to 'value'
+var value = 42;
+//2. Create condition logic to check if the value is great or equal to 53
+//2-1. If true, add 42 to 'value'
+//2-2. If false, subtract 13 from 'value'
 
-3. Create a string that is set to 11, add it to 'value'
+if(value >= 53) {
+  value += 42;
+} else {
+  value -= 13;
+};
 
-4. Create an array, loop through 'value' using charAt, set array[i] to each value
+console.log(value);
 
-5. Remove the first and last values off the array
+//3. Create a string that is set to 11, add it to 'value'
 
-6. Create a new variable. Loop backwards through the array and store each value into the new Variable, combining each of the values of that array (backwards remember!)
+value = (value + "11");
+console.log(value);
 
-7. parseInt both the 'value' and the new variable created in Step 6, ensure that both 'value' and the new variable are set to these new parsed values;
+//4. Create an array, loop through 'value' using charAt, set array[i] to each value
 
-******* SWITCH PROGRAMMERS *******
+var array = [];
+for(var i = 0; i < value.length; i++){
+   array[i] = value.charAt(i);
 
-8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
+}
 
-9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27. 
-If neither of these are true, set the value to 2.
+console.log(array);
 
-10. Create a while loop that counts down from 10 and increments 'value' by 1.
+//5. Remove the first and last values off the array
 
-11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there 
-is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
+console.log(array.shift());
+console.log(array.pop());
 
-12. Call the function.
+console.log(array);
 
-13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.
+//6. Create a new variable. Loop backwards through the array and store each value
+//into the new Variable, combining each of the values of that array (backwards remember!)
 
-14. Your answer should be a String value that equals 7. Is that what you got?
-*/
+
+var newVariable = "";
+
+for(var i = (array.length - 1); i >= 0; i--) {
+  newVariable += array[i];
+  array[i] = array[i];
+}
+
+
+console.log(newVariable);
+console.log(value);
+
+//7. parseInt both the 'value' and the new variable created in Step 6, ensure that both 'value' and the new variable are set to these new parsed values;
+
+value = parseInt(value);
+newVariable = parseInt(newVariable);
+
+console.log(newVariable + " " + value);
+//******* SWITCH PROGRAMMERS *******
+
+//8. Add 'value' and the new variable created in Step 6 together and store them in 'value'
+
+value = (value + newVariable);
+console.log(value);
+
+//9. If the new value of 'value' is less than 60, set 'value' equal to 14. If not, check to see if it is equal to 2930, if it is, set 'value' equal to 27.
+//If neither of these are true, set the value to 2.
+
+if(value < 60){
+  value = 14;
+} else if(value == 2930){
+  value = 27;
+} else {
+  value = 2
+};
+console.log(value);
+
+//10. Create a while loop that counts down from 10 and increments 'value' by 1. // POTENTIAL PROBLEM
+i = 10;
+while(i > 0){
+value++;
+i--;
+  }
+  console.log(value)
+
+
+//11. Create a function that accepts an argument 'val'. The function should convert 'val' to a String, then drop the first character off the String, but only if there
+//is more than 1 character in the String. Return 'val' and set the 'value' to the returned value.
+function aFunction(val) {
+val = val.toString();
+
+if(val.length > 1){
+val = val.substr(1);
+}
+console.log(val);
+return val;
+}
+
+
+//12. Call the function.
+value = aFunction(value);
+//13. Console log value. Create an index.html file, link the this Javascript file. Load in the browser, check the value.
+console.log(value);
+//14. Your answer should be a String value that equals 7. Is that what you got?
+//*/
